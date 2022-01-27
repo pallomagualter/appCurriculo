a2caf1191d1722fe1872777af42e1f529a341679
pallomagualter <pallomagualter@gmail.com>
2022-01-27 11:47:41 -0300

    using props

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,24 +3,12 @@ import { Text, View } from 'react-native';
 
 import style from './style';
 
-const Card = (props) => {
+const Card = ({ titulo, children }) => {
     return (
         <View style={style.card_container}>
             <View style={style.card}>
                 <View style={style.card_header}>
-                    <Text>Experiência Profissional</Text>
-                </View>
-                <View style={style.card_content}>
-                    <Text style={style.card_content_text}>Secretaria de Estado da Educação - SEDUC-PI</Text>
-                    <Text style={style.card_content_text}>Adebaran Studio</Text>
-                    <Text style={style.card_content_text}>Bem Brasil LTDA (Atuando na PRF-PI)</Text>
-                    <Text style={style.card_content_text}>Agência de Tecnologia da Informação do Estado do Piauí</Text>
-                </View>
-            </View>
-
-            <View style={style.card}>
-                <View style={style.card_header}>
-                    <Text>Formação Acadêmica</Text>
+                    <Text>{titulo}</Text>
                 </View>
                 <View style={style.card_content}>
                     <Text style={style.card_content_text}>UNIVERSIDADE FEDERAL DO PIAUÍ - UFPI</Text>
