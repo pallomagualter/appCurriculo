21643f6a4ff9b155ce6c69a174eb5362b65b2844
pallomagualter <pallomagualter@gmail.com>
2022-01-27 09:24:47 -0300

    body layout

--- src/App.js
+++ src/App.js
@@ -1,49 +1,42 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Image,
-  Text,
-  View,
-} from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import foto from './assets/foto.jpg';
 
 const App = () => {
-
   return (
     <>
       <View style={styles.page}>
-        <View style={styles.container_cabecalho} >
+        <View style={styles.container_cabecalho}>
           <Image source={foto} style={styles.foto} />
           <Text style={styles.nome}>PALLOMA GUALTER</Text>
           <Text style={styles.funcao}> Desenvolvedora Mobile</Text>
-          <View>
-            <Icon name="github" />
-            <Text style={styles.social}>Instagram</Text>
-            <Text style={styles.social}>LinkedIn</Text>
+          <View style={styles.redes_sociais}>
+            <Icon name="github" size={30} />
+            <Icon name="instagram" size={30} />
+            <Icon name="linkedin" size={30} />
           </View>
         </View>
       </View>
     </>
-
   );
 };
 
 const styles = StyleSheet.create({
   page: {
     backgroundColor: '#E7E7E7',
-    flex: 1
+    flex: 1,
   },
   container_cabecalho: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
   },
   foto: {
     width: 250,
     height: 250,
-    borderRadius: 125
+    borderRadius: 125,
   },
   nome: {
     marginTop: 10,
@@ -52,11 +45,14 @@ const styles = StyleSheet.create({
   },
   funcao: {
     color: '#939393',
-    marginBottom: 10
+    marginBottom: 10,
   },
-  social: {
-
-  }
+  redes_sociais: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
+    marginTop: 20,
+  },
 });
 
 export default App;
